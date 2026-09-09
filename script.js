d0902af813faf19a8c7fd6c25255e1cdbf508bb6
AniketMenkudle <aniketmenkudle94@gmail.com>
2026-09-09 13:32:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+
+const buttons = document.querySelectorAll(".btn");
+const stopButton = document.querySelector(".stop");
+
+let currentAudio = null;
+
+buttons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+
+        // Stop previous sound
+        if (currentAudio) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+        }
+
+        // Get sound name
+        const soundName = button.dataset.sound;
+
+        // Create audio
+        currentAudio = new Audio(`sounds/${soundName}.mp3`);
+
+        // Play sound
+        currentAudio.play();
+    });
+
+});
+
+
+// Stop button
+stopButton.addEventListener("click", () => {
+
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+
+});
+```
